71b7fa74995df4b2bb21f06ad95e1c34b039806e
yas3jb <yasmu213@gmail.com>
2023-10-19 15:42:15 -0400

    Added unit and functional tests

---
+++ script.js
@@ -0,0 +1,22 @@
+//* Unit Tests
+
+// 'Multiplication' Function
+
+// Expect the product of (5, 5) to be a number.
+// Expect the product of (5, 5) to return 25.
+// Expect the product ("a", 5) to be an error.
+
+// 'concatOdds' Function
+
+// Expect the two arrays concatOdds ([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to be a whole numbers.
+// Expect the two arrays concatOdds ([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to be an arguments.
+// Expect the two arrays concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to be returned as single array [-1, 1, 3, 9, 15].
+// Expect concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to be in ascending order [-1, 1, 3, 9, 15].
+
+
+
+//* Functional Test
+
+// When the user clicks "cart" without adding items, they should be directed to page showing an empty cart.
+// When the user clicks "cart" but has filled the cart, they should be dirceted to page showing all the items they added in the cart.
+// When the user clicks "checkout" they should be prompted to "login", "create an account" or "continue as a guest".
